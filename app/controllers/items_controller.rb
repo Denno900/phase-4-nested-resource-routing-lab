d75bb86b917336a434ba36726a4e9636c0280564
dennis mwaniki <dennomaz@gmail.com>
2023-03-20 20:46:29 +0300

    assignment done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,23 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+      render json: items, include: :user
+    else
+      render json: Item.all, include: :user
+    end
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    newItem = Item.create(name: params[:name],description: params[:description],price: params[:price], user_id: params[:user_id])
+    render json: newItem, status: :created
   end
 
 end
